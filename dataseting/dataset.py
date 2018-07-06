11626145550fe905f1814d99b0cd6aa4ee55ab29
Page-David <david991010@gmail.com>
2018-07-06 20:19:53 +0800

    add docstring

--- dataseting/dataset.py
+++ dataseting/dataset.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
+"""Different datasets structures are defined here."""
 
 
 class Dataset(object):
+    """A brief framework of each dataset"""
 
     def __init__(self):
+        """Basic variables in dataset class"""
         self.dataset_size = int()
         self.data = list()
         self.labels = list()
 
     def concatence(self):
+        """Method used to concatence two datasets."""
         pass
 
     def transform_data(self):
+        """Method used to transform dataset data
+        with given function.
+        """
         pass
 
     def transform_labels(self):
+        """Method used to transform dataset labels
+        with given function.
+        """
         pass
